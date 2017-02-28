d031684e8797a055782343438aefdad8371de409
Chloe <chloejchou@gmail.com>
2017-02-27 17:05:47 -0800

    add time complexity comments

--- algorithms/practice-thy-algorithms-master/Ruby/lib/algorithms.rb
+++ algorithms/practice-thy-algorithms-master/Ruby/lib/algorithms.rb
@@ -5,26 +5,32 @@ require 'set'
 # Keep repeating until there is only one digit in the result.
 # The result is called a 'digital root'.
 # Do not use string conversion within your method.
+
+# 0(1)
 def digital_root(number)
   return number if number < 10
   sum = 0
-  number.to_s.split("").each do |num|
-    sum += num.to_i
+  until number == 0
+    sum += number % 10
+    number /= 10
   end
 
   digital_root(sum)
 end
 
+# p digital_root(119)
+
 # Write a function that takes a message and an increment amount.
 # Output the same letters shifted by that amount in the alphabet.
 # Assume lowercase and no punctuation.
 # Preserve spaces.
+
+# 0(n)
 def caesar_cipher(string, shift)
   answer = []
   string.split("").each do |letter|
     new_ord = ((letter.ord + shift) % 123)
     new_ord += 97 if new_ord < 97
-    puts new_ord
     answer << new_ord.chr
   end
 
@@ -35,6 +41,8 @@ end
 
 # Write a function that takes two strings.
 # Return the longest common substring.
+
+# 0(n^2 * m)
 def common_substrings(string_one, string_two)
   answer = ""
   start_idx = 0
@@ -44,7 +52,6 @@ def common_substrings(string_one, string_two)
       substring = string_one[start_idx..end_idx]
       if string_two.include?(substring)
         answer = substring if answer.length < substring.length
-        puts answer
       end
       end_idx += 1
     end
@@ -59,6 +66,8 @@ end
 
 # Write a function that takes an array of integers and returns their sum.
 # Use recursion.
+
+# 0(n)
 def sum_rec(numbers)
   return numbers[0] if numbers.length == 1
   numbers[0] + sum_rec(numbers[1..-1])
@@ -68,6 +77,8 @@ end
 
 # Write a function that takes n, the length of the sequence.
 # Return the first n elements from the fibonnacci sequence as an array.
+
+# 0(n)
 def fibs(n)
   return [] if n < 1
   return [1] if n == 1
@@ -84,6 +95,8 @@ end
 # Write a function that takes a string.
 # Return true if the string is a palindrome, otherwise return false.
 # It should take less time and memory than reversing the string.
+
+# 0(n)
 def is_palindrome?(string)
   el_one = 0
   el_two = -1
@@ -106,6 +119,8 @@ end
 # Write a method that takes a string as input.
 # It should return true if the input is a valid IPv4 address.
 # Valid IPs are anything between '0.0.0.0' and '255.255.255.255'.
+
+# 0(1)
 def valid_ip?(string)
   address = string.split(".")
   return false if address.length != 4
@@ -123,6 +138,8 @@ end
 # Implement the Folding Cipher.
 # It folds the alphabet in half and uses the adjacent letter.
 # a -> z, b -> y, c -> x, m -> n, etc...
+
+# 0(n)
 def folding_cipher(string)
   hash = {}
   alphabet = ('a'..'z').to_a
@@ -143,6 +160,8 @@ end
 # p folding_cipher("abc")
 
 # Write a method that finds all the unique substrings for a word.
+
+# 0(n^3)
 def uniq_subs(string)
   subs = []
   start_idx = 0
@@ -164,6 +183,8 @@ end
 # Given an array of integers find the largest contiguous subsum.
 # You can solve this trivially in O(n**2) time by considering all subarrays.
 # Try to solve it in O(n) time with O(1) memory.
+
+# 0(n)
 def lcs(array)
   current_sum = 0
   largest_sum = array[0]
@@ -182,6 +203,8 @@ end
 # Write a function that takes a year as a four digit integer.
 # Returns an array of the 10 closest subsequent silly years.
 # A silly year's first two digits plus the last two digits equal the middle two.
+
+# 0(1)
 def silly_years(year)
   answer = []
   current_year = year
@@ -203,17 +226,18 @@ end
 # List the pairs in [min, max] order.
 # Time complexity: O(n).
 # Return a set.
+
+# 0(n)
 def pair_sum(array, k)
   set = Set.new
-  hash = Hash.new
-  array.each do |el|
-    hash[el] = k - el
-  end
+  seen = Set.new
 
   array.each do |el|
-    if array.include?(hash[el])
-      set << [el, hash[el]].sort
+    target = k - el
+    if seen.include?(target)
+      set << [el, target].sort
     end
+    seen << el
   end
 
   set
@@ -245,6 +269,8 @@ end
 
 # Implement Merge Sort
 # Hint: This typically involves a helper function.
+
+# 0(nlog(n))
 def merge_sort(array)
   return array if array.length == 1
   mid = array.length / 2
@@ -273,6 +299,8 @@ end
 
 # Implement binary search.
 # Return nil if the target isn't found.
+
+# 0(log(n))
 def binary_search(array, target)
   return nil if array.length < 2 && array[0] != target
   mid = array.length / 2
@@ -291,6 +319,7 @@ end
 # You are given a list of numbers in an array.
 # Replace all the numbers with the product of all other numbers.
 # Do this in O(n) time without using division.
+
 def productify(array)
   answer = []
   array.each_with_index do |el, idx|
@@ -377,57 +406,123 @@ def can_win?(array, index, seen = {})
   can_win?(array, index + step_size, seen) || can_win?(array, index - step_size, seen)
 end
 
+# p can_win?([1,2,4,0], 2)
+
 # Assume an array of length n, containing the numbers 1..n in jumbled order.
 # "Sort" this array in O(n) time.
 # Hint: You should be able to do this without looking at the input.
 def sort1(array)
-
+  (1..array.length).to_a
 end
+
+# p sort1([7,8,3,9,5,6,1,2,4])
 
 # Assume an array of length n with numbers in the range 1..N (N >= n).
 # Sort this array in O(n + N) time.
 # You may use O(N) memory.
 def sort2(array, max_value)
+  hash = {}
+  array.each do |el|
+    hash[el] = true
+  end
+  answer = []
+  (1..max_value).each do |el|
+    answer << el if hash[el]
+  end
 
+  answer
 end
+
+# p sort2([7,8,3,9,5,6,10,2], 10)
 
 # Say I give you an array of n strings, each of length k.
 # Merge sort can sort this in O(knlog(n)).
 # Sort the strings in O(kn).
 # Hint: Do not compare any two strings.
 # All strings contain only lowercase letters without whitespace or punctuation.
-def sort3(array, length)
+def sort3(strings, length)
+  (length - 1).downto(0) do |i|
+    # a bucket for each letter of the alphabet
+    buckets = Array.new(26) { [] }
+    strings.each do |string|
+      letter = string[i]
+      # push the words into each bucket
+      buckets[letter.ord - "a".ord] << string
+    end
 
+    strings = []
+    buckets.each do |bucket|
+      bucket.each { |string| strings << string }
+    end
+  end
+
+  strings
 end
 
 # Given an array, write a function that will return a random index of the array.
 # The chance of returning a given index will vary with the value of the element.
 # Probability of i should be the ith element divided by the sum of all elements.
 def weighted_random_index(array)
-
+  sample = []
+  array.each_with_index do |el, idx|
+    el.times { sample << idx }
+  end
+  sample.sample
 end
+
+# p weighted_random_index([1,2,3,4,5])
 
 # Given an array, move all zeros to the end.
 # The order of non-zero elements does not matter.
 # Try to accomplish this in O(n) time and O(1) space.
 def move_zeros(array)
+  barrier = 0
+  array.each_with_index do |el, idx|
+    if el == 0
+      array[barrier], array[idx] = array[idx], array[barrier]
+      barrier += 1
+    end
+  end
 
+  array
 end
+
+# p move_zeros([0,1,2,3,4,0,6,9,0])
 
 # Implement the 'look and say' function.
 #'Look and say' takes an input array and outputs an array.
 # The output describes the count of the elements in the input.
 
 def look_and_say(array)
+  current_el = array[0]
+  count = 0
+  answer = []
+  array.each do |el|
+    if el == current_el
+      count += 1
+    else
+      answer << [count, current_el]
+      count = 1
+      current_el = el
+    end
+  end
+  answer << [count, current_el]
 
+  answer
 end
+
+# p look_and_say([1,1,2,1,1,2])
 
 # I give you a scrambled list of n unique integers between 0 and n.
 # Tell me what number is missing?
 # How could you solve the problem in O(n), and also O(1) space?
 def sum_upon_sums(array)
-
+  sum = (0..array.length).reduce(:+)
+  sum_array = array.reduce(:+)
+  sum - sum_array
 end
+
+# p sum_upon_sums([0,1,2,3,4,5])
 
 # Implement a stack with a max method that returns the maximum value.
 # It should run in O(1) time.
@@ -523,8 +618,55 @@ end
 # Write a MinMaxStackQueue which tracks both the min and max.
 # Last, use MinMaxStackQueue to solve the problem.
 class MinMaxStack
+  attr_accessor :stack, :max_stack, :min_stack
 
+  def initialize
+    @stack = []
+    @max_stack = []
+    @min_stack = []
+  end
+
+  def remove
+    @max_stack.pop
+    @min_stack.pop
+    @stack.pop
+  end
+
+  def add(item)
+    current_max = @max_stack[-1] || -1.0 / 0.0
+    current_min = @min_stack[-1] || 1.0 / 0.0
+    @stack << item
+    if item > current_max
+      @max_stack << item
+    else
+      @max_stack << current_max
+    end
+
+    if item < current_min
+      @min_stack << item
+    else
+      @min_stack << current_min
+    end
+
+    @stack
+  end
+
+  def max
+    @max_stack[-1]
+  end
+
+  def min
+    @min_stack[-1]
+  end
 end
+
+# max_min = MinMaxStack.new
+# max_min.add(1)
+# max_min.add(2)
+# max_min.add(4)
+# max_min.add(1)
+# max_min.add(0)
+# p max_min.min
 
 class MinMaxStackQueue
 
@@ -541,28 +683,75 @@ end
 # Write a function that takes such a hash.
 # Return an array of strings with the path to each file in the hash.
 def file_list(hash)
+  paths = []
+  hash.keys.each do |key|
+    if hash[key] == true
+      paths << key
+    else
+      file_list(hash[key]).each do |path|
+        paths << key + path
+      end
+    end
+  end
 
+  paths
 end
+
+# p file_list({
+#   "a" => {"b" => true, "c" => {"d" => true}},
+#   "e" => true
+#   })
 
 # Assume an array of non-negative integers.
 # A second array is made by shuffling the first and deleting a random element.
 # Given these two arrays, find which element is missing in the second array.
 # Do this in linear time with constant memory use.
 def find_missing_number(array_one, array_two)
-
+  sum_one = array_one.reduce(:+)
+  sum_two = array_two.reduce(:+)
+  (sum_one - sum_two).abs
 end
+
+# p find_missing_number([1,2,3],[3,1])
 
 # Create a function that takes three strings.
 # Return whether the third is an interleaving of the first two.
 # Interleaving means it contains the same characters and preserves their order.
 def is_shuffle?(string_one, string_two, string_three)
-
+  string_one_arr = string_one.split("")
+  string_two_arr = string_two.split("")
+  current_idx = 0
+  until string_one_arr.empty? || string_two_arr.empty?
+    if string_one_arr[0] == string_three[current_idx]
+      string_one_arr.shift
+    elsif string_two_arr[0] == string_three[current_idx]
+      string_two_arr.shift
+    else
+      return false
+    end
+    current_idx += 1
+  end
+  # p string_three[0...current_idx]
+  string_three[0...current_idx] + string_one_arr.join("") + string_two_arr.join("") == string_three
 end
+
+# p is_shuffle?("hl", "elo", "hello")
+# p is_shuffle?("godb", "oye", "goodbye")
+# p is_shuffle?("asfsd", "f", "asdfsdf")
 
 # Write a function that takes an integer and returns it in binary form.
-def binary(integer)
+# 156 -> 10011100
+def binary(num)
+  result = []
+  until num == 0
+    result.unshift(num % 2)
+    num /= 2
+  end
 
+  result.join
 end
+
+p binary(156)
 
 # Write a recursive function that takes a number and returns its factorial.
 def recursive_factorial(number)
